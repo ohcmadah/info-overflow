9b5b18917bfa01b060973a15f0a9c5f148a04900
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-05 23:24:55 +0900

    admin, settings

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import ugettext_lazy as _
+
+from .forms import UserCreationForm, UserChangeForm
+from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('get_full_name', 'email', 'name', 'id', 'department', 'is_active', 'is_superuser',)
+    list_display_links = ('get_full_name',)
+    list_filter = ('is_superuser', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('id', 'password')}),
+        (_('Personal info'), {'fields': ('name', 'email', 'department')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_superuser',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email','name')
+    ordering = ('name',)
+    filter_horizontal = ()
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
