ae7ee617737d62962897bb61f9be8ece6411b16a
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-05 23:06:49 +0900

    account User, UserManager

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+# SIGN UP
+def signup(request):
+    if request.method == 'POST':
+        for p in request.POST:
+            if p != "":
+                if p['password'] == p['password-check']:
+                    user = User.objects.create_user(
+                        username=p['name'],
+                        password=p['password']
+
+                    )
