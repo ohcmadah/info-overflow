2e7d284a927c5eb3fe07c64cfd6a4e41cfc49727
cho-hadam <s2019w16@e-mirim.hs.kr>
2020-11-10 13:41:12 +0900

    댓글 보이기, 댓글 수

--- blog/models.py
+++ blog/models.py
@@ -32,10 +32,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     published_date = models.DateTimeField()
 
